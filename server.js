40f8997033b6553548f4aa3facfa337d6cde4c8a
kuerim <kuerim@taltech.ee>
2024-03-26 06:53:26 +0200

    week10-Axios

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    let url = 'https://api.themoviedb.org/3/movie/6114?api_key=d168777b89b9d5bfd3c3322bd505fd36';
+    axios.get(url)
+    .then(response => {
+        let data = response.data;
+        let releaseDate = new Date(data.release_date).getFullYear();
+
+        let genresToDisplay = '';
+        data.genres.forEach(genre => {
+            genresToDisplay = genresToDisplay + `${genre.name}, `;
+        });
+
+        let genresUpdated = genresToDisplay.slice(0, -2) + '.';
+        let posterUrl = `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+
+        let currentYear = new Date().getFullYear();
+        res.render('index', {
+            dataToRender: data, 
+            year: currentYear,
+            releaseYear: releaseDate,
+            genres: genresUpdated,
+            poster: posterUrl
+        });
+        app.listen(process.env.PORT || 3000, () => {
+            console.log('server is running');
+        });
+           
+    });
+    
+});
+
+app.listen(3000, ()=> {
+    console.log('Server is running on Port 3000.');
+});
